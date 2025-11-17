d9893e117ba90b530760da7fda3292e6c499803f
mellivorandy <mellivorandy@gmail.com>
2025-11-17 09:55:23 +0800

    feat(UI): add Animation on play/pause button

---
+++ ZeroTouch.UI/Converters/BoolTrueToOneConverter.cs
@@ -0,0 +1,15 @@
+using Avalonia.Data.Converters;
+using System;
+using System.Globalization;
+
+namespace ZeroTouch.UI.Converters
+{
+    public class BoolTrueToOneConverter : IValueConverter
+    {
+        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+            => (value is bool b && b) ? 1.0 : 0.0;
+
+        public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+            => throw new NotSupportedException();
+    }
+}
