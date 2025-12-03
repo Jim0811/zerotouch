3386b7c6b35cc7689ee61db0a58882d122705b00
mellivorandy <mellivorandy@gmail.com>
2025-12-03 23:42:31 +0800

    feat(UI): add map view

--- ZeroTouch.UI/Views/MainDashboardView.axaml.cs
+++ ZeroTouch.UI/Views/MainDashboardView.axaml.cs
@@ -1,6 +1,14 @@
 using Avalonia.Controls;
 using Avalonia.Controls.Primitives;
+using System.Linq;
 using ZeroTouch.UI.ViewModels;
+using Mapsui;
+using Mapsui.Extensions;
+using Mapsui.Projections;
+using Mapsui.Tiling;
+using Mapsui.UI.Avalonia;
+using Mapsui.Widgets;
+using Mapsui.Widgets.ScaleBar;
 
 namespace ZeroTouch.UI.Views
 {
@@ -9,6 +17,8 @@ namespace ZeroTouch.UI.Views
         public MainDashboardView()
         {
             InitializeComponent();
+
+            InitializeMap();
         }
 
         private void MusicSlider_OnValueChanged(object? sender, RangeBaseValueChangedEventArgs e)
@@ -17,6 +27,34 @@ namespace ZeroTouch.UI.Views
             {
                 vm.SeekCommand.Execute((long)e.NewValue);
             }
+        }
+
+        private void InitializeMap()
+        {
+            var mapControl = this.FindControl<MapControl>("MapControl");
+            if (mapControl == null) return;
+
+            var map = new Map();
+
+            map.Layers.Add(OpenStreetMap.CreateTileLayer());
+
+            // Remove default widgets
+            while (map.Widgets.TryDequeue(out _)) { }
+
+            mapControl.Map = map;
+
+            // Ensure the map is loaded before performing operations
+            mapControl.Loaded += (s, e) =>
+            {
+                // Get coordinates
+                var coords = SphericalMercator.FromLonLat(120.2845411717568, 22.73356348850075);
+
+                var point = new MPoint(coords.x, coords.y);
+
+                // Navigate to the point
+                map.Navigator.CenterOn(point);
+                map.Navigator.ZoomTo(2.0);
+            };
         }
     }
 }
